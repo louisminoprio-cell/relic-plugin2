524afa295094816b59d90499a215b3fc1875460e
louisminoprio-cell <louis.minoprio@gmail.com>
2026-06-05 21:34:29 +0100

    Update ExecutionerRelic.java

--- src/main/java/dev/relicforging/relic/ExecutionerRelic.java
+++ src/main/java/dev/relicforging/relic/ExecutionerRelic.java
@@ -42,7 +42,7 @@ public class ExecutionerRelic extends Relic {
 
             player.getWorld().playSound(
         player.getLocation(),
-        org.bukkit.Sound.BLOCK.RESPAWN_ANCHOR.CHARGE,
+        org.bukkit.Sound.RESPAWN_ANCHOR.CHARGE,
         1f,
         1f
     );
